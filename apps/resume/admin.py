1de27413b13e1ae19483f1bcc3c3cf0306cf9098
Alex Boag-Munroe <alex.munroe@zpg.co.uk>
2018-11-11 14:50:11 +0000

    Add Resume model view to admin panel

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title')
+
 @admin.register(models.ResumeItem)
 class ResumeItemAdmin(admin.ModelAdmin):
     list_display = ('resume', 'title', 'company', 'start_date')
